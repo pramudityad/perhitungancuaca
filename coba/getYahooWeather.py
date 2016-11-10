0edf08d9fe126e03f7b68621de8d903ba7a0795f
Aris Pujud Kurniawan <aris.pujud@gmail.com>
2016-11-11 01:04:49 +0700

    add coba

---
+++ coba/getYahooWeather.py
@@ -0,0 +1,7 @@
+import urllib2, urllib, json
+baseurl = "https://query.yahooapis.com/v1/public/yql?"
+yql_query = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=\"Bandung, id\")"
+yql_url = baseurl + urllib.urlencode({'q':yql_query}) + "&format=json"
+result = urllib2.urlopen(yql_url).read()
+data = json.loads(result)
+print data['query']['results']['channel']['wind']['speed']
