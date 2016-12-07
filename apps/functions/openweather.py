9c6f6b684e1ee33a8b71e02e70b00b8d5293fe4a
Aris Pujud Kurniawan <aris.pujud@gmail.com>
2016-12-07 17:20:36 +0700

    add parsing on openweather function

--- apps/functions/openweather.py
+++ apps/functions/openweather.py
@@ -1,6 +1,43 @@
 import urllib2, urllib, json
+import datetime, time
+from datetime import timedelta
+
 def getForecast():
-    url    = 'http://api.openweathermap.org/data/2.5/forecast?q=Bandung&appid=ab09346d9a9123104405c6a84ad48c19'
+    appid  = 'ab09346d9a9123104405c6a84ad48c19';
+    host   = 'http://api.openweathermap.org/';
+    lat    = '-8.0831578';
+    lon    = '111.7092587';
+    units  = 'metric';
+    #url    = 'http://api.openweathermap.org/data/2.5/forecast?q=Bandung&appid=ab09346d9a9123104405c6a84ad48c19'
+    url    = host+'data/2.5/forecast?lat='+lat+'&lon='+lon+'&appid='+appid+'&units='+units;
     result = urllib2.urlopen(url).read()
     data   = json.loads(result)
     return data;
+
+def getCityName(data):
+    return data['city']['name'];
+
+def getCityLatitude(data):
+    return data['city']['coord']['lat'];
+
+def getCityLongitude(data):
+    return data['city']['coord']['lon'];
+def getList(data):
+    #for var in data['list']:
+        #print(var['dt_txt']);
+    return data['list'];
+def getForecastNext(data):
+    myTime  = datetime.datetime.now();
+    hour    = myTime.hour;
+    while(hour%3!=0):
+        hour = hour-1;
+        myTime -= timedelta(hours=1);
+    myTime += timedelta(hours=3);
+    timeRequest = myTime.strftime('%Y-%m-%d %H:00:00');
+    #print timeRequest;
+    res = '';
+    for var in data['list']:
+        if(timeRequest == var['dt_txt']):
+            res = var;
+    return res;
+    
--- apps/main.py
+++ apps/main.py
@@ -2,12 +2,28 @@ import urllib2, urllib, json
 import functions.openweather as OW
 import datetime, time
 
+
 print "Start"
+now = datetime.datetime.now();
+timeRequest = now.strftime('%Y-%m-%d %H:%M:%S');
+str_ow_data = OW.getForecast();
+print 'Time      : ' + timeRequest;
+print 'Lokasi    : ' + OW.getCityName(str_ow_data);
+print 'Latitude  : ' + str(OW.getCityLatitude(str_ow_data));
+print 'Longitude : ' + str(OW.getCityLongitude(str_ow_data));
+print 'Prediksi  : Jam      :' + str(OW.getForecastNext(str_ow_data)['dt_txt']);
+print '            Cuaca    :' + str(OW.getForecastNext(str_ow_data)['weather'][0]['description']);
+print '            Code     :' + str(OW.getForecastNext(str_ow_data)['weather'][0]['id']);
+
 while (1):
     now = datetime.datetime.now()
-    #print now.hour, now.minute, now.second
+    timeRequest = now.strftime('%Y-%m-%d %H:%M:%S');
+    #print now.year, now.hour, now.minute, now.second
     if(now.hour%1==0 and now.minute%2==0 and now.second==0):
+        print 'Request New Data';
+        str_ow_data = OW.getForecast();
         #print OW.getForecast();
-        print "Request at: ",now.hour,":",now.minute,":",now.second
-    time.sleep(1)
+        #print "Request at: ",now.hour,":",now.minute,":",now.second
+    print timeRequest + '\t' + OW.getCityName(str_ow_data)+'\t' + str(OW.getCityLatitude(str_ow_data))+'\t'+ str(OW.getCityLongitude(str_ow_data))+ '\t' + str(OW.getForecastNext(str_ow_data)['dt_txt'])+'\t' + str(OW.getForecastNext(str_ow_data)['weather'][0]['description'])+'\t' + str(OW.getForecastNext(str_ow_data)['weather'][0]['id']);
+    #time.sleep(1)
     
